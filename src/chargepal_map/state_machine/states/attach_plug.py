96744e4dc7d3c34527e53dcdad91883d746d5eb5
Georg John <georg.john@dfki.de>
2024-06-10 18:39:05 +0200

    Update state AttachPlug

--- src/chargepal_map/state_machine/states/attach_plug.py
+++ src/chargepal_map/state_machine/states/attach_plug.py
@@ -37,14 +37,13 @@ class AttachPlug(State):
 
     def execute(self, ud: Any) -> str:
         print(state_header(type(self))) 
-        rospy.loginfo('Start attaching the robot to the plug')
+        # Get user and configuration data
         job: Job = ud.job
-        # Get transformation matrices
         T_base2socket = sm.SE3(ud.T_base2socket)
-        # Get plug type key
-        plug_type = job.get_plug_type()
+        rospy.loginfo('Start to try attaching the robot to the plug')
+        sus_cup_plug, sus_dec_plug, sus_lock_plug = False, False, False
         # Start attaching procedure
-        with self.pilot.plug_model.context(plug_type):
+        with self.pilot.plug_model.context(plug_type=job.get_plug_type()):
             with self.pilot.context.force_control():
                 sus_cup_plug, lin_ang_err = self.pilot.try2_couple_to_plug(
                     T_base2socket=T_base2socket,
@@ -66,10 +65,28 @@ class AttachPlug(State):
                     rospy.loginfo(f"Lock robot with plug successfully: {sus_lock_plug}")
                     rospy.logdebug(f"Final error after locking robot with plug: "
                                    f"(Linear error={lin_ang_err[0]}[m] | Angular error={lin_ang_err[1]}[rad])")
-        if not sus_cup_plug or not sus_lock_plug:
-            raise StateMachineError(f"Spatial error to large. Robot is probably in an undefined condition.")
+                else:
+                    sus_dec_plug, lin_ang_err = self.pilot.try2_decouple_to_plug(
+                        time_out=self.cfg.data['decouple_time_out'],
+                        max_force=self.cfg.data['decouple_max_force'],
+                        max_torque=self.cfg.data['decouple_max_torque'],
+                        decoupling_tolerance=self.cfg.data['decouple_tolerance']
+                        )
+        if sus_cup_plug and sus_lock_plug:
+            outcome = out.plug_attached
+        elif not sus_cup_plug and not sus_lock_plug and sus_dec_plug:
+            if job.retry_count > 1:
+                outcome = out.err_obs_plug_recover
+                rospy.loginfo(f"Robot was not able to attach to the plug. Try to recover the arm")
+            else:
+                job.retry()
+                outcome = out.err_plug_out_retry
+                rospy.loginfo(f"Robot was not able to attach to the plug. Retry attachment procedure")
+        elif not sus_cup_plug and not sus_dec_plug or not sus_lock_plug:
+            outcome = out.err_plug_in_stop
+            rospy.logerr(f"Robot got stuck while trying to attach. Arm is in an undefined condition.")
         if self.user_cb is not None:
-            outcome = self.user_cb.request_action(out.plug_attached, out.job_stopped)
+            outcome = self.user_cb.request_action(out.outcome, out.job_stopped)
         job.track_state(type(self))
         print(state_footer(type(self)))
         return outcome
