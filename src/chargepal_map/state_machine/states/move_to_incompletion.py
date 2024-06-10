8e94301136934ba8ebe005cedf61608e401fbdb1
Georg John <georg.john@dfki.de>
2024-06-10 15:18:55 +0200

    Implement state MoveToIncompletion

--- src/chargepal_map/state_machine/states/move_to_incompletion.py
+++ src/chargepal_map/state_machine/states/move_to_incompletion.py
@@ -9,7 +9,11 @@ from chargepal_map.job import Job
 from chargepal_map.state_machine import outcomes as out
 from chargepal_map.state_machine.step_by_user import StepByUser
 from chargepal_map.state_machine.state_config import StateConfig
+from chargepal_map.state_machine.states.attach_plug import AttachPlug
+from chargepal_map.state_machine.states.observe_plug import ObservePlug
+from chargepal_map.state_machine.states.observe_plug_scene import ObservePlugScene
 from chargepal_map.state_machine.utils import (
+    state_name,
     state_header,
     state_footer,
     StateMachineError,
@@ -34,10 +38,36 @@ class MoveToIncompletion(State):
     def execute(self, ud: Any) -> str:
         print(state_header(type(self)))
         job: Job = ud.job
-        wps = self.cfg.data[job.get_id()]['joint_waypoints']
+        # Find matching key for motion path configuration
+        if job.latest_state() == state_name(ObservePlugScene):
+            state_key = 'observe_plug_scene'
+        elif job.latest_state() == state_name(ObservePlug):
+            state_key = 'observe_plug'
+        elif job.latest_state() == state_name(AttachPlug):
+            state_key = 'attack_plug'
+        else:
+            raise StateMachineError(f"Latest state '{job.latest_state()}' cannot be matched to a new state outcome")
+        
+        if job.is_part_of_plug_in() and job.is_part_of_workspace_left():
+            job_key = 'home_position_workspace_left'
+        elif job.is_part_of_plug_in() and job.is_part_of_workspace_right():
+            job_key = 'home_position_workspace_left'
+        elif job.is_part_of_plug_out() and job.is_part_of_type2_male():
+            use_way_pts = True
+            job_key = 'joint_waypoints_bcs2home'
+        elif job.is_part_of_plug_out() and job.is_part_of_type2_female():
+            use_way_pts = True
+            job_key = 'joint_waypoints_ads2home_ls'
+        elif job.is_part_of_plug_out() and job.is_part_of_ccs_female():
+            use_way_pts = True
+            job_key = 'joint_waypoints_ads2home_rs'
+        else:
+            raise StateMachineError(f"Current job '{job.ID}' cannot be matched to a new state outcome")
+
         rospy.loginfo(f"Start moving the arm to a save driving position.")
+        act_values = self.cfg.data[state_key][job_key]
         with self.pilot.context.position_control():
-            self.pilot.robot.move_path_j(wps, self.cfg.data['vel'], self.cfg.data['acc'])
+            self.pilot.robot.move_path_j(act_values, self.cfg.data['vel'], self.cfg.data['acc'])
         rospy.loginfo(f"Arm ended in joint configuration: {ur_pilot.utils.vec_to_str(self.pilot.robot.joint_pos)}")
         outcome = out.job_incomplete
         job.track_state(type(self))
