ae9476524bd70da88044ef3777a2ec54001419ad
Georg John <georg.john@dfki.de>
2024-06-12 15:31:57 +0200

    Add recovery behaviour

--- src/chargepal_map/state_machine/states/observe_socket.py
+++ src/chargepal_map/state_machine/states/observe_socket.py
@@ -41,30 +41,43 @@ class ObserveSocket(State):
     def execute(self, ud: Any) -> str:
         print(state_header(type(self)))
         job: Job = ud.job
-        if job.in_stop_mode() or job.in_recover_mode():
-            raise StateMachineError(f"Job {job} in an invalid mode. Interrupt process")
         found_socket = False
-        if job.is_part_of_plug_out():
-            T_base2socket_close_up = job.interior_socket.T_base2socket_close_up
+        if job.in_progress_mode() or job.in_retry_mode():
+            rospy.loginfo('Start observing the socket from a close-up view')
+            if job.is_part_of_plug_out():
+                T_base2socket_close_up = job.interior_socket.T_base2socket_close_up
+                if T_base2socket_close_up is None:
+                    raise StateMachineError(f"Missing observation of the socket. Interrupt process")
+                found_socket = True
+            elif job.is_part_of_plug_in():
+                T_base2socket_scene = job.exterior_socket.T_base2socket_scene
+                if T_base2socket_scene is None:
+                    raise StateMachineError(f"Missing observation of the plug scene. Interrupt process")
+                socket_dtt = self.cfg.data['detector'][job.get_plug_type()]['detector']
+                with self.pilot.plug_model.context(plug_type=job.get_plug_type()):
+                    with self.pilot.context.position_control():
+                        self.pilot.set_tcp(ur_pilot.EndEffectorFrames.CAMERA)
+                        T_base2camera = T_base2socket_scene * self._T_socket_save2camera
+                        self.pilot.robot.movel(T_base2camera, self.cfg.data['vel'], self.cfg.data['acc'])
+                    found_socket, T_base2socket_close_up = self.pilot.find_target_pose(
+                        detector_fp=socket_dtt, time_out=self.cfg.data['detector_time_out'])
+                if found_socket:
+                    job.exterior_socket.T_base2socket_close_up = sm.SE3(T_base2socket_close_up)
+            else:
+                raise StateMachineError(f"Invalid or undefined job '{job}' for this state")
+        elif job.in_recover_mode():
+            rospy.loginfo('Skip observation process. Should be already done before')
+            if job.is_part_of_plug_in():
+                T_base2socket_close_up = job.interior_socket.T_base2socket_close_up
+            elif job.is_part_of_plug_out():
+                T_base2socket_close_up = job.exterior_socket.T_base2socket_close_up
+            else:
+                raise StateMachineError(f"Invalid or undefined job '{job}' for this state")
             if T_base2socket_close_up is None:
                 raise StateMachineError(f"Missing observation of the socket. Interrupt process")
             found_socket = True
-        elif job.is_part_of_plug_in():
-            T_base2socket_scene = job.exterior_socket.T_base2socket_scene
-            if T_base2socket_scene is None:
-                raise StateMachineError(f"Missing observation of the plug scene. Interrupt process")
-            socket_dtt = self.cfg.data['detector'][job.get_plug_type()]['detector']
-            with self.pilot.plug_model.context(plug_type=job.get_plug_type()):
-                with self.pilot.context.position_control():
-                    self.pilot.set_tcp(ur_pilot.EndEffectorFrames.CAMERA)
-                    T_base2camera = T_base2socket_scene * self._T_socket_save2camera
-                    self.pilot.robot.movel(T_base2camera, self.cfg.data['vel'], self.cfg.data['acc'])
-                found_socket, T_base2socket_close_up = self.pilot.find_target_pose(
-                    detector_fp=socket_dtt, time_out=self.cfg.data['detector_time_out'])
-            if found_socket:
-                job.exterior_socket.T_base2socket_close_up = sm.SE3(T_base2socket_close_up)
-        else:
-            raise StateMachineError(f"Invalid or undefined job '{job}' for this state")
+        else:  # job.in_stop_mode():
+            raise StateMachineError(f"Job {job} in an invalid mode. Interrupt process")
         if found_socket:
             rospy.loginfo(f"Found a socket pose.")
             rospy.logdebug(f"Transformation: Base-Socket = {ur_pilot.utils.se3_to_str(T_base2socket_close_up)}")
