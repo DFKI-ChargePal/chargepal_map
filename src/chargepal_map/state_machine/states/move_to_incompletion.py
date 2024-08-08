679270cf4c06fa97ad84b84d2d1badae7afe9c43
Gurunatraj <gurunatraj.parthasarathy@dfki.de>
2024-08-08 14:46:51 +0200

    Change key pattern for movements

--- src/chargepal_map/state_machine/states/move_to_incompletion.py
+++ src/chargepal_map/state_machine/states/move_to_incompletion.py
@@ -43,43 +43,49 @@ class MoveToIncompletion(State):
         print(state_header(type(self)))
         job: Job = ud.job
         # Find matching key for motion path configuration
+        plug_type_key = job.get_plug_type()
         if job.latest_state() == state_name(ObserveBattery):
             state_key = 'observe_battery'
+            p2p_key = 'battery2home'
         elif job.latest_state() == state_name(ObservePeriphery):
             state_key = 'observe_periphery'
+            p2p_key = 'periphery2home'
         elif job.latest_state() == state_name(ObservePlugId):
             state_key = 'observe_plug_id'
+            if job.is_part_of_plug_in():
+                p2p_key = 'battery2home'
+            elif job.is_part_of_plug_out():
+                p2p_key = 'periphery2home'
+            else:
+                raise StateMachineError(f"Current job '{job.ID}' cannot be matched to a waypoint set")
         elif job.latest_state() == state_name(ObservePlug):
             state_key = 'observe_plug'
+            p2p_key = 'periphery2home'
         elif job.latest_state() == state_name(AttachPlug):
             state_key = 'attach_plug'
+            if job.is_part_of_plug_in():
+                p2p_key = 'battery2home'
+            elif job.is_part_of_plug_out():
+                p2p_key = 'periphery2home'
+            else:
+                raise StateMachineError(f"Current job '{job.ID}' cannot be matched to a waypoint set")
         elif job.latest_state() == state_name(ReleasePlug):
             state_key = 'release_plug'
+            if job.is_part_of_plug_out():
+                p2p_key = 'battery2home'
+            elif job.is_part_of_plug_in():
+                p2p_key = 'periphery2home'
+            else:
+                raise StateMachineError(f"Current job '{job.ID}' cannot be matched to a waypoint set")
         else:
             raise StateMachineError(f"Latest state '{job.latest_state()}' cannot be matched to a new state outcome")
-
-        if job.is_part_of_plug_in() and job.is_part_of_workspace_left():
-            job_key = 'home_position_workspace_left'
-        elif job.is_part_of_plug_in() and job.is_part_of_workspace_right():
-            job_key = 'home_position_workspace_left'
-        elif job.is_part_of_plug_out() and job.is_part_of_type2_male():
-            use_way_pts = True
-            job_key = 'joint_waypoints_bcs2home'
-        elif job.is_part_of_plug_out() and job.is_part_of_type2_female():
-            use_way_pts = True
-            job_key = 'joint_waypoints_ads2home_ls'
-        elif job.is_part_of_plug_out() and job.is_part_of_ccs_female():
-            use_way_pts = True
-            job_key = 'joint_waypoints_ads2home_rs'
-        else:
-            raise StateMachineError(f"Current job '{job.ID}' cannot be matched to a new state outcome")
         outcome = ''
         if self.user_cb is not None:
             rospy.loginfo(f"Ready to move arm in final position")
             outcome = self.user_cb.request_action(outcome, out.job_stopped)
         if outcome != out.job_stopped:
             rospy.loginfo(f"Start moving the arm to a save driving position.")
-            act_values = self.cfg.data[state_key][job_key]
+            act_values = self.cfg.data[plug_type_key][state_key][p2p_key]
             with self.pilot.context.position_control():
                 self.pilot.robot.move_path_j(act_values, self.cfg.data['vel'], self.cfg.data['acc'])
             rospy.loginfo(f"Arm ended in joint configuration: {ur_pilot.utils.vec_to_str(self.pilot.robot.joint_pos)}")
